cbbf7cac0b9111a49b26deb62c4e756d0084a44d
ebiyy <kuraim22@gmail.com>
2022-06-04 05:36:54 +0900

    add: monorepo

---
+++ apps/server/functions/index.ts
@@ -0,0 +1,10 @@
+import * as functions from "firebase-functions";
+import {
+  initializeApp,
+} from "firebase-admin/app";
+initializeApp();
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
